f5de1307bfe2cdc2bba8e2f3be05c08c6331a077
adil-elbikha <elbikha_adil@emsi-edu.ma>
2021-07-13 16:59:40 +0100

    configuration md-1

---
+++ src/main/java/ma/emsi/tpjpa/GestionStockWebApplication.java
@@ -0,0 +1,13 @@
+package ma.emsi.tpjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionStockWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionStockWebApplication.class, args);
+	}
+
+}
